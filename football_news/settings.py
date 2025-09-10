679a5c17631fd82fa8b1069630e4b3dfbdca3ed2
freyazahra <freyazahra08@icloud.com>
2025-09-10 15:30:12 +0700

    fix: final

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "freya-zahra-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/freya.zahra/footballshop"
+    "https://freya-zahra-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
